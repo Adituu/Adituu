b1adcd9a95d33b75ec95f7d44ef20ba3d74a8490
Adituu <46101581+Adituu@users.noreply.github.com>
2022-05-30 18:13:40 +0300

    removed utils.py

--- flaskr/core/utils.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-
-def get_current_year():
-    return datetime.datetime.now().year
--- flaskr/routes/index.py
+++ flaskr/routes/index.py
@@ -1,16 +1,8 @@
 import flask
 
-from flaskr.core.utils import (
-    get_current_year
-)
-
 blueprint = flask.Blueprint(name='index', import_name=__name__)
 
 
 @blueprint.get('/')
 def index():
-    render_args = {
-        'year': get_current_year()
-    }
-
-    return flask.render_template('index.html', **render_args), 200
+    return flask.render_template('index.html'), 200
